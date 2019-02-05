ab10a5467d59e278d69b2e7a61e7a3bbd653f870
Matt-Dahlseid <matt.dahlseid@gmail.com>
2019-02-04 16:44:58 -0800

    Through Lesson 16

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -18,7 +18,7 @@ export function addComment(postId, author, comment) {
 }
 
 // remove comment
-export function removeComments(postId, i) {
+export function removeComment(postId, i) {
     return {
         type: 'REMOVE_COMMENT',
         postId,
