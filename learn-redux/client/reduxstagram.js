2c68090d256b9b6d352f7bf7c9b173e12fbcf005
Matt-Dahlseid <matt.dahlseid@gmail.com>
2019-02-06 18:29:34 -0800

    Through Lesson 18

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -13,23 +13,6 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
-import Raven from 'raven-js';
-import { sentry_url, logException } from './data/config';
-
-Raven.config(sentry_url, {
-    tags: {
-        git_commit: 'asdfas9d08f',
-        userLevel: 'editor'
-    }
-}).install();
-
-logException(new Error('download failed!'), {
-    email: 'wesbos@gmail.com'
-});
-
-Raven.captureMessage('Something bad happened!');
-Raven.showReportDialog();
-
 const router = (
     <Provider store={store}>
         <Router history={history}>
